10d51f49102ad4404c0c67b6acc37e7b7ba5244d
TarhunchiKKK <kostabarilo12@gmail.com>
2025-12-07 17:25:39 +0300

    feat: Overlay component

--- src/features/board-editor/index.tsx
+++ src/features/board-editor/index.tsx
@@ -4,6 +4,7 @@ import { useRef } from "react";
 import { Canvas } from "./ui/canvas";
 import { Dots } from "./ui/dots";
 import { Layout } from "./ui/layout";
+import { Overlay } from "./ui/overlay";
 
 export function BoardEditor() {
     const canvasRef = useRef<HTMLDivElement>(null);
@@ -12,7 +13,7 @@ export function BoardEditor() {
         <Layout>
             <Dots />
 
-            <Canvas ref={canvasRef} overlay={<div></div>}></Canvas>
+            <Canvas ref={canvasRef} overlay={<Overlay />}></Canvas>
         </Layout>
     );
 }
