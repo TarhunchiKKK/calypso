faf6913432b23e2f59d569aa9940219019a37b29
Konstantin Barilo <kostabarilo12@gmail.com>
2025-12-08 12:36:11 +0000

    test: NodesFactory.sticker

--- test/features/board-editor/nodes-factory.spec.ts
+++ test/features/board-editor/nodes-factory.spec.ts
@@ -6,7 +6,8 @@ import { Sticker } from "@/features/board-editor/nodes/variants/sticker";
 describe("NodesFactory", () => {
     describe("create", () => {
         test("should create a node with given type and properties", () => {
-            const nodes = [
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            const nodes: { input: AnyNode; expectedClass: new (...args: any[]) => object }[] = [
                 {
                     input: {
                         type: "sticker",
@@ -27,6 +28,25 @@ describe("NodesFactory", () => {
                 expect(node).toBeInstanceOf(expectedClass);
                 expect(node.id).toBe(input.id);
             }
+        });
+    });
+
+    describe("sticker", () => {
+        test("should create a sticker node with correct properties", () => {
+            const inputNode: AnyNode = {
+                type: "sticker",
+                id: "sticker1",
+                x: 50,
+                y: 75,
+                width: 150,
+                height: 80,
+                text: "Hello Sticker"
+            };
+
+            const node = NodesFactory.create(inputNode);
+
+            expect(node).toBeInstanceOf(Sticker);
+            expect(node.id).toBe(inputNode.id);
         });
     });
 });
