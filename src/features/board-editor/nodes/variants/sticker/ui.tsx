fd2540821945f0e6f134829e338552d83e20d706
TarhunchiKKK <kostabarilo12@gmail.com>
2025-12-11 22:27:05 +0300

    refactor: move resize borders to separate component

--- src/features/board-editor/nodes/variants/sticker/ui.tsx
+++ src/features/board-editor/nodes/variants/sticker/ui.tsx
@@ -3,6 +3,7 @@ import { CSSProperties } from "react";
 import { NodeHandlers } from "../base";
 import { StickerNode } from "./type";
 import { ResizeDirection } from "@/features/board-editor/domain/dom";
+import { ResizeBorders } from "@/features/board-editor/ui/resizing-borders";
 
 type Props = {
     node: StickerNode;
@@ -34,43 +35,8 @@ export function StickerComponent({ node, isSelected, resizable, handlers }: Prop
             )}
             style={styles}
         >
-            {resizable && (
-                <>
-                    <div
-                        className="absolute -left-2 w-4 h-full cursor-w-resize"
-                        onMouseDown={e => handleResizeStart("w", e)}
-                    ></div>
-                    <div
-                        className="absolute -right-2  w-4 h-full cursor-w-resize z-20"
-                        onMouseDown={e => handleResizeStart("e", e)}
-                    ></div>
-                    <div
-                        className="absolute -top-2 w-full h-4 cursor-n-resize"
-                        onMouseDown={e => handleResizeStart("n", e)}
-                    ></div>
-                    <div
-                        className="absolute -bottom-2 w-full h-4 cursor-n-resize"
-                        onMouseDown={e => handleResizeStart("s", e)}
-                    ></div>
+            {resizable && <ResizeBorders main cross diagonal handleResizeStart={handleResizeStart} />}
 
-                    <div
-                        className="absolute -left-1 -top-1 w-2 h-2 rounded-full cursor-nw-resize  bg-blue-700"
-                        onMouseDown={e => handleResizeStart("nw", e)}
-                    ></div>
-                    <div
-                        className="absolute -right-1 -top-1 w-2 h-2 rounded-full cursor-ne-resize  bg-blue-700"
-                        onMouseDown={e => handleResizeStart("ne", e)}
-                    ></div>
-                    <div
-                        className="absolute -right-1 -bottom-1 w-2 h-2 rounded-full cursor-se-resize  bg-blue-700"
-                        onMouseDown={e => handleResizeStart("se", e)}
-                    ></div>
-                    <div
-                        className="absolute -left-1 -bottom-1 w-2 h-2 rounded-full cursor-sw-resize  bg-blue-700"
-                        onMouseDown={e => handleResizeStart("sw", e)}
-                    ></div>
-                </>
-            )}
             {node.text}
         </div>
     );
---
+++ src/features/board-editor/ui/resizing-borders.tsx
@@ -0,0 +1,65 @@
+import { ResizeDirection } from "../domain/dom";
+
+type Props = {
+    main?: boolean;
+
+    cross?: boolean;
+
+    diagonal?: boolean;
+
+    handleResizeStart: (direction: ResizeDirection, e: React.MouseEvent) => void;
+};
+
+export function ResizeBorders({ main, cross, diagonal, handleResizeStart }: Props) {
+    return (
+        <>
+            {main && (
+                <>
+                    <div
+                        className="absolute -top-2 w-full h-4 cursor-n-resize"
+                        onMouseDown={e => handleResizeStart("n", e)}
+                    ></div>
+                    <div
+                        className="absolute -bottom-2 w-full h-4 cursor-n-resize"
+                        onMouseDown={e => handleResizeStart("s", e)}
+                    ></div>
+                </>
+            )}
+
+            {cross && (
+                <>
+                    <div
+                        className="absolute -left-2 w-4 h-full cursor-w-resize"
+                        onMouseDown={e => handleResizeStart("w", e)}
+                    ></div>
+
+                    <div
+                        className="absolute -right-2  w-4 h-full cursor-w-resize z-20"
+                        onMouseDown={e => handleResizeStart("e", e)}
+                    ></div>
+                </>
+            )}
+
+            {diagonal && (
+                <>
+                    <div
+                        className="absolute -left-1 -top-1 w-2 h-2 rounded-full cursor-nw-resize  bg-blue-700"
+                        onMouseDown={e => handleResizeStart("nw", e)}
+                    ></div>
+                    <div
+                        className="absolute -right-1 -top-1 w-2 h-2 rounded-full cursor-ne-resize  bg-blue-700"
+                        onMouseDown={e => handleResizeStart("ne", e)}
+                    ></div>
+                    <div
+                        className="absolute -right-1 -bottom-1 w-2 h-2 rounded-full cursor-se-resize  bg-blue-700"
+                        onMouseDown={e => handleResizeStart("se", e)}
+                    ></div>
+                    <div
+                        className="absolute -left-1 -bottom-1 w-2 h-2 rounded-full cursor-sw-resize  bg-blue-700"
+                        onMouseDown={e => handleResizeStart("sw", e)}
+                    ></div>
+                </>
+            )}
+        </>
+    );
+}
