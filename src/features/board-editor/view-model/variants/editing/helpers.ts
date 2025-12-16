b51ede4026dfbe84a5d3f38b9a1fbcd75c415ae5
kostabarilo12 <kostabarilo12@gmail.com>
2025-12-16 10:39:21 +0000

    refactor: editing view model simplification

---
+++ src/features/board-editor/view-model/variants/editing/helpers.ts
@@ -0,0 +1,29 @@
+import { NodesMapper } from "@/features/board-editor/domain/nodes-mapping";
+import { NodeImpl } from "@/features/board-editor/nodes/variants/base";
+import { EditingViewState } from "./view-state";
+import React from "react";
+
+export class EditingNodesMapper extends NodesMapper {
+    private constructor(
+        nodes: NodeImpl[],
+        private viewState: EditingViewState
+    ) {
+        super(nodes);
+    }
+
+    public static from(nodes: NodeImpl[], viewState: EditingViewState) {
+        return new EditingNodesMapper(nodes, viewState);
+    }
+
+    public applyHandlers(endEditingHandler: (node: NodeImpl) => void, clickHandler: (e: React.MouseEvent) => void) {
+        this.nodes = this.nodes.map(node => {
+            if ((this.viewState.selectedNodeId = node.id)) {
+                return node.clone().select().setEditing().setHandler("onEditingEnd", endEditingHandler);
+            }
+
+            return node.setHandler("onClick", clickHandler);
+        });
+
+        return this;
+    }
+}
--- src/features/board-editor/view-model/variants/editing/view-model.ts
+++ src/features/board-editor/view-model/variants/editing/view-model.ts
@@ -1,20 +1,20 @@
+import { withNodeId } from "@/features/board-editor/domain/dom";
 import { ViewModel, ViewModelParams } from "../../types";
 import { switchToIdle } from "../idle/switcher";
 import { switchToSelection } from "../selection/switcher";
 import { EditingViewState } from "./view-state";
+import { EditingNodesMapper } from "./helpers";
 
 export function useEditingViewModel({ nodesModel, setViewState }: ViewModelParams) {
     return (viewState: EditingViewState): ViewModel => {
-        const handleSwitchToSelection = (nodeId: string) => {
+        const handleClick = withNodeId(nodeId => {
             setViewState(switchToSelection({ selectedIds: new Set([nodeId]), skipNextClick: true }));
-        };
+        });
 
         return {
-            nodes: nodesModel.nodes.map(node =>
-                viewState.selectedNodeId === node.id
-                    ? node.clone().select().setEditing().setHandler("onEditingEnd", nodesModel.updateOne)
-                    : node.setHandler("onClick", () => handleSwitchToSelection(node.id))
-            ),
+            nodes: EditingNodesMapper.from(nodesModel.nodes, viewState)
+                .applyHandlers(nodesModel.updateOne, handleClick)
+                .get(),
             overlay: {
                 onClick: () => setViewState(switchToIdle())
             }
