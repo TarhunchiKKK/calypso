f92e22cc2eeb3889644a46923a1f7c38c5e85719
Konstantin Barilo <kostabarilo12@gmail.com>
2025-12-22 12:53:18 +0000

    stories: ActionButton

---
+++ stories/board-editor/ui/action-button.stories.tsx
@@ -0,0 +1,29 @@
+import { ActionButton } from "@/features/board-editor/ui/action-bar";
+import { Meta, StoryObj } from "@storybook/nextjs-vite";
+import { StickerIcon } from "lucide-react";
+import { centered } from "../../common/center";
+
+const meta = {
+    title: "Board Editor/ui/Action Button",
+    component: ActionButton
+} satisfies Meta<typeof ActionButton>;
+
+export default meta;
+
+export const Default: StoryObj<typeof meta> = {
+    args: {
+        isActive: false,
+        onClick: () => {},
+        children: <StickerIcon />
+    },
+    decorators: centered
+};
+
+export const Active: StoryObj<typeof meta> = {
+    args: {
+        isActive: true,
+        onClick: () => {},
+        children: <StickerIcon />
+    },
+    decorators: centered
+};
