ac93d317b2b70c6cfc41737ff10ab670f730d347
Konstantin Barilo <kostabarilo12@gmail.com>
2025-12-22 12:49:21 +0000

    stories: Dots

--- stories/board-editor/ui/selection-window.stories.tsx
+++ stories/board-editor/ui/selection-window.stories.tsx
@@ -1,6 +1,5 @@
 import { SelectionWindow } from "@/features/board-editor/ui/selection-window";
 import { Meta, StoryObj } from "@storybook/nextjs-vite";
-import { centered } from "../../common/center";
 
 const meta = {
     title: "Board Editor/ui/Selection Window",
@@ -15,6 +14,5 @@ export const Default: StoryObj<typeof meta> = {
         y: 100,
         width: 200,
         height: 200
-    },
-    decorators: centered
+    }
 };
