7c2286c7ef8b790eb58b5b07ee02744dd63ab844
Konstantin Barilo <kostabarilo12@gmail.com>
2025-12-22 12:56:52 +0000

    stories: ActionsBar

---
+++ stories/board-editor/ui/actions-bar.stories.tsx
@@ -0,0 +1,26 @@
+import { ActionButton, ActionsBar } from "@/features/board-editor/ui/action-bar";
+import { Meta, StoryObj } from "@storybook/nextjs-vite";
+import { MousePointer2, StickerIcon } from "lucide-react";
+
+const meta = {
+    title: "Board Editor/ui/Actions Bar",
+    component: ActionsBar
+} satisfies Meta<typeof ActionsBar>;
+
+export default meta;
+
+export const Default: StoryObj<typeof meta> = {
+    args: {
+        children: (
+            <>
+                <ActionButton isActive={false} onClick={() => {}}>
+                    <MousePointer2 />
+                </ActionButton>
+
+                <ActionButton isActive={true} onClick={() => {}}>
+                    <StickerIcon />
+                </ActionButton>
+            </>
+        )
+    }
+};
