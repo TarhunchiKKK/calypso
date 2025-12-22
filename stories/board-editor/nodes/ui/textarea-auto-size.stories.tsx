4177d046742873c1005c0e32eb0e61a72f8eec8a
Konstantin Barilo <kostabarilo12@gmail.com>
2025-12-22 12:34:01 +0000

    stories: TextareaAutoSize

---
+++ stories/board-editor/nodes/ui/textarea-auto-size.stories.tsx
@@ -0,0 +1,38 @@
+import { TextareaAutoSize } from "@/features/board-editor/nodes/ui/textarea-auto-size";
+import { Meta, StoryObj } from "@storybook/nextjs-vite";
+import { centered } from "../../../common/center";
+
+const meta = {
+    title: "BoardEditor/nodes/ui/TextareaAutoSize",
+    component: TextareaAutoSize
+} satisfies Meta<typeof TextareaAutoSize>;
+
+export default meta;
+
+const renderFn = (args: StoryObj<typeof meta>["args"]) => {
+    return (
+        <div className="w-72 h-72">
+            <TextareaAutoSize {...args} />
+        </div>
+    );
+};
+
+export const Default: StoryObj<typeof meta> = {
+    args: {
+        isActive: false,
+        value: "Value",
+        onEditingEnd: () => {}
+    },
+    decorators: centered,
+    render: renderFn
+};
+
+export const Active: StoryObj<typeof meta> = {
+    args: {
+        isActive: true,
+        value: "Value",
+        onEditingEnd: () => {}
+    },
+    decorators: centered,
+    render: renderFn
+};
