9c23f1d7847d04cb503c7b10b97a73e702342215
Konstantin Barilo <kostabarilo12@gmail.com>
2025-12-22 09:30:12 +0000

    init: Storybook

---
+++ stories/ui/button.stories.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/shared/ui/kit/button";
+import { Meta, StoryObj } from "@storybook/nextjs-vite";
+
+const meta = {
+    title: "Ui/Kit/Button",
+    component: Button
+} satisfies Meta<typeof Button>;
+
+export default meta;
+
+export const Primary: StoryObj<typeof meta> = {
+    args: {
+        type: "button",
+        size: "lg"
+    }
+};
