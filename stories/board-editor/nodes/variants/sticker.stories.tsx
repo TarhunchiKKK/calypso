a7d1a25b2480413b449cd39d5006cb0107803994
Konstantin Barilo <kostabarilo12@gmail.com>
2025-12-22 12:41:39 +0000

    stories: StickerComponent

---
+++ stories/board-editor/nodes/variants/sticker.stories.tsx
@@ -0,0 +1,56 @@
+import { StickerComponent } from "@/features/board-editor/nodes/variants/sticker/ui";
+import { Meta, StoryObj } from "@storybook/nextjs-vite";
+import { centered } from "../../../common/center";
+
+const meta = {
+    title: "BoardEditor/nodes/variants",
+    component: StickerComponent
+} satisfies Meta<typeof StickerComponent>;
+
+export default meta;
+
+const node = { id: "id", type: "sticker", x: 100, y: 100, width: 200, height: 200, text: "Hello" } as const;
+
+export const Default: StoryObj<typeof meta> = {
+    args: {
+        node: node,
+        isSelected: false,
+        resizable: false,
+        isEditing: false,
+        handlers: {}
+    },
+    decorators: centered
+};
+
+export const Selected: StoryObj<typeof meta> = {
+    args: {
+        node: node,
+        isSelected: true,
+        resizable: false,
+        isEditing: false,
+        handlers: {}
+    },
+    decorators: centered
+};
+
+export const Resizable: StoryObj<typeof meta> = {
+    args: {
+        node: node,
+        isSelected: false,
+        resizable: true,
+        isEditing: false,
+        handlers: {}
+    },
+    decorators: centered
+};
+
+export const Editing: StoryObj<typeof meta> = {
+    args: {
+        node: node,
+        isSelected: false,
+        resizable: false,
+        isEditing: true,
+        handlers: {}
+    },
+    decorators: centered
+};
