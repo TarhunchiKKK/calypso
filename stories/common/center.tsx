27cee3a0dcbc5d2798537009f7d69fa16924ee9d
Konstantin Barilo <kostabarilo12@gmail.com>
2025-12-24 08:08:45 +0000

    fix: fix knip errors

--- stories/common/center.tsx
+++ stories/common/center.tsx
@@ -1,14 +1,9 @@
 import { StoryObj } from "@storybook/nextjs-vite";
-import { PropsWithChildren } from "react";
-
-export function Center({ children }: PropsWithChildren) {
-    return <div className="w-full h-screen flex flex-col justify-center items-center">{children}</div>;
-}
 
 export const centered: StoryObj["decorators"] = Story => {
     return (
-        <Center>
+        <div className="w-full h-screen flex flex-col justify-center items-center">
             <Story />
-        </Center>
+        </div>
     );
 };
