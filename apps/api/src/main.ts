e1e88ac974c72e345a023dd20a74ef5502948450
kostabarilo12 <kostabarilo12@gmail.com>
2026-01-21 09:14:10 +0000

    chore: turborepo scripts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory } from "@nestjs/core";
 
-import { AppModule } from './app.module';
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
-  app.enableCors();
-  
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    app.enableCors();
+
+    await app.listen(3000);
 }
 
 void bootstrap();
