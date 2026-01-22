d0f827d36c5975d6a021c5ffba69186962dc9929
kostabarilo12 <kostabarilo12@gmail.com>
2026-01-22 06:13:34 +0000

    feat: `ZodValidationPipe`

--- apps/api/src/modules/auth/auth.controller.ts
+++ apps/api/src/modules/auth/auth.controller.ts
@@ -1,27 +1,35 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Delete, Get, Post, UsePipes } from "@nestjs/common";
+import { AuthDto } from "@repo/common";
+import { ZodValidationPipe } from "src/shared/validation";
 import type { AuthService } from "./auth.service";
 import { Authorization } from "./decorators/authorization.decorator";
 import { Authorized } from "./decorators/authorized.decorator";
 import type { SignInRequest } from "./dto/sign-in.dto";
 import type { SignUpRequest } from "./dto/sign-up.dto";
 
-@Controller("basic")
+@Controller("auth")
 export class AuthController {
     public constructor(private readonly authService: AuthService) {}
 
+    @Post("sign-up")
+    @UsePipes(new ZodValidationPipe(AuthDto))
     public async signUp(request: SignUpRequest) {
         return await this.authService.signUp(request);
     }
 
+    @Post("sign-in")
+    @UsePipes(new ZodValidationPipe(AuthDto))
     public async signIn(request: SignInRequest) {
         return await this.authService.signIn(request);
     }
 
+    @Get()
     @Authorization()
     public async findOne(@Authorized("username") username: string) {
         return await this.authService.findOne(username);
     }
 
+    @Delete()
     @Authorization()
     public async removeOne(@Authorized("username") username: string) {
         return await this.authService.removeOne(username);
