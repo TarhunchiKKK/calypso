c4392419ce21cc60b86972af6bc08fd68be32f2f
kostabarilo12 <kostabarilo12@gmail.com>
2026-01-28 10:52:01 +0000

    feat(api): `ReplaceOneNodeCommandHandler`

--- apps/api/src/modules/nodes/dto/replace-node.dto.ts
+++ apps/api/src/modules/nodes/dto/replace-node.dto.ts
@@ -2,4 +2,4 @@ import { PickType } from "@nestjs/swagger";
 import type { ReplaceNodeBaseDto as TypeReplaceNodeDto } from "@repo/common";
 import { NodeApiType } from "../swagger/node.api-type";
 
-export class Replace extends PickType(NodeApiType, ["id", "boardId", "type", "blocked"] as const) implements TypeReplaceNodeDto {}
+export class ReplaceNodeDto extends PickType(NodeApiType, ["id", "boardId", "type", "blocked"] as const) implements TypeReplaceNodeDto {}
