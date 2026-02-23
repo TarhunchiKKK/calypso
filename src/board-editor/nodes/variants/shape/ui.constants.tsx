25ce7f7523fa80a210213eaa4b0a2c98dab2e266
kostabarilo12 <kostabarilo12@gmail.com>
2026-02-23 12:20:49 +0000

    fix: add sizes for shape node icons

--- src/board-editor/nodes/variants/shape/ui.constants.tsx
+++ src/board-editor/nodes/variants/shape/ui.constants.tsx
@@ -2,10 +2,10 @@ import { Circle, Diamond, Hexagon, Square, Star, Triangle } from "lucide-react";
 import type { ShapeVariants } from "./shape-node.type";
 
 export const ShapeVariantIconsMap: Record<ShapeVariants, React.ReactNode> = {
-    rectangle: <Square />,
-    circle: <Circle />,
-    triangle: <Triangle />,
-    diamond: <Diamond />,
-    star: <Star />,
-    hexagon: <Hexagon />
+    rectangle: <Square className="w-full h-full" />,
+    circle: <Circle className="w-full h-full" />,
+    triangle: <Triangle className="w-full h-full" />,
+    diamond: <Diamond className="w-full h-full" />,
+    star: <Star className="w-full h-full" />,
+    hexagon: <Hexagon className="w-full h-full" />
 };
