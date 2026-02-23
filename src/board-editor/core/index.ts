83c716ca1de926b6609e8b43d2177b922deecff0
kostabarilo12 <kostabarilo12@gmail.com>
2026-02-23 08:53:36 +0000

    ci: move all Docker files into single folder

--- src/board-editor/core/index.ts
+++ src/board-editor/core/index.ts
@@ -5,5 +5,5 @@ export { getNodeId, withNodeId } from "./lib/dom.lib";
 export { Geometry } from "./lib/geometry.facade";
 export { NodesMapper } from "./lib/nodes-mapper.class";
 export type { Decoratable } from "./types/decorators.types";
-export type { Renderable } from "./types/ui.types";
 export type { NodeBase, NodeStyles, NodeTypes, RectNode } from "./types/node.types";
+export type { Renderable } from "./types/ui.types";
