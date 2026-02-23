0e9be79e9db891d027384136f6c6cb3304348161
kostabarilo12 <kostabarilo12@gmail.com>
2026-02-23 09:01:38 +0000

    feat: refresh-env script

---
+++ scripts/refresh-env-example.ts
@@ -0,0 +1,24 @@
+import fs from "fs";
+import path from "path";
+
+const rootDir = process.cwd();
+const sourceEnvFile = path.join(rootDir, ".env");
+const destinationFile = path.join(rootDir, ".env.example");
+
+if (!fs.existsSync(sourceEnvFile)) {
+    console.error(`Error: Source file not found at ${sourceEnvFile}`);
+    process.exit(1);
+}
+
+// load environment variables to example environment variables file
+const envContent: string = fs.readFileSync(sourceEnvFile, "utf-8");
+const envKeys = envContent.split("\n").map(line => {
+    if (line.includes("=")) {
+        return `${line.split("=")[0]}=`;
+    } else {
+        return line;
+    }
+});
+
+const exampleEnvContent = envKeys.join("\n");
+fs.writeFileSync(destinationFile, exampleEnvContent);
