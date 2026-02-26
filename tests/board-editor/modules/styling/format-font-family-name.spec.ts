95fefb5eae22b5ce48be62951a617930a14b4b9d
kostabarilo12 <kostabarilo12@gmail.com>
2026-02-26 08:53:40 +0000

    test: `formatFontFamilyName` function

---
+++ tests/board-editor/modules/styling/format-font-family-name.spec.ts
@@ -0,0 +1,31 @@
+import { describe, expect, test } from "vitest";
+import { formatFontFamilyName } from "@/board-editor/modules/styling/lib/utils";
+
+describe("`formatFontFamilyName` function", () => {
+    test("formats font family name", () => {
+        const dataset = [
+            {
+                input: "",
+                output: ""
+            },
+            {
+                input: "roboto-mono",
+                output: "Roboto Mono"
+            },
+            {
+                input: "arial",
+                output: "Arial"
+            },
+            {
+                input: "times-new-roman",
+                output: "Times New Roman"
+            }
+        ];
+
+        for (const data of dataset) {
+            const result = formatFontFamilyName(data.input);
+
+            expect(result).toEqual(data.output);
+        }
+    });
+});
