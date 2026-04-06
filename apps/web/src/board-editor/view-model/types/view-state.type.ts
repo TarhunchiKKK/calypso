c6a18a9abab488e96093d6dc6937ef019e1de8fb
Konstantin Barilo <kostabarilo12@gmail.com>
2026-04-06 09:47:07 +0000

    feat(apps/web): `useTextCreationViewModel`

--- apps/web/src/board-editor/view-model/types/view-state.type.ts
+++ apps/web/src/board-editor/view-model/types/view-state.type.ts
@@ -1,4 +1,5 @@
 import type { ArrowBindingViewState } from "../variants/arrow-binding/view-state";
+import type { ArrowCreationViewState } from "../variants/arrow-creating/view-state";
 import type { DraggingViewState } from "../variants/dragging/view-state";
 import type { EditingViewState } from "../variants/editing/view-state";
 import type { IdleViewState } from "../variants/idle/view-state";
@@ -10,13 +11,14 @@ import type { ShapeSelectionViewState } from "../variants/shape-selection/view-s
 import type { ShapesCreationViewState } from "../variants/shapes-creation/view-state";
 import type { StickersCreationViewState } from "../variants/stickers-creation/view-state";
 import type { StylingViewState } from "../variants/styling/view-state";
-import type { ArrowCreationViewState } from "../variants/arrow-creating/view-state";
+import type { TextCreationViewState } from "../variants/text-creation/view-state";
 
 export type ViewState =
     | IdleViewState
     | StickersCreationViewState
     | ArrowCreationViewState
     | ArrowBindingViewState
+    | TextCreationViewState
     | ShapeSelectionViewState
     | ShapesCreationViewState
     | SelectionViewState
