394741496234ae03c3d6e1a3d19f73b4472e188f
Konstantin Barilo <kostabarilo12@gmail.com>
2026-04-07 07:31:19 +0000

    feat(apps/web): `SignUpPage` and `SignInPage` pages

--- apps/web/src/shared/config/routing.constants.ts
+++ apps/web/src/shared/config/routing.constants.ts
@@ -3,6 +3,10 @@ import type { Id, ProjectTypes } from "@repo/common";
 export const Routes = {
     home: "/",
     dashboard: "/dashboard",
+    auth: {
+        signUp: "/auth/sign-up",
+        signIn: "/auth/sign-in"
+    },
     apps: {
         board: (id: Id) => `board/${id}`,
         note: (id: Id) => `note/${id}`
