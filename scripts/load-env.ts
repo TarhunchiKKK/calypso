5759d483bba546221334215c3da9e9a88872f104
Konstantin Barilo <kostabarilo12@gmail.com>
2026-04-13 12:23:10 +0000

    chore: update `scripts/load-env.ts` script

--- scripts/load-env.ts
+++ scripts/load-env.ts
@@ -9,76 +9,190 @@ if (!fs.existsSync(sourceEnvFile)) {
     process.exit(1);
 }
 
-// mapping global `.env` key names to service `.env` key names
-const map = {
-    gateway: {
-        title: "Api Gateway",
-        destination: path.join(rootDir, "apps/gateway/.env"),
-        variables: {
-            APP_PORT: "GATEWAY_SERVICE_APP_PORT",
+type Apps = "gateway" | "boards" | "media" | "web";
 
-            COOKIE_HTTP_ONLY: "COOKIE_HTTP_ONLY",
-            COOKIE_SECURE: "COOKIE_SECURE",
-            ACCESS_COOKIE_EXPIRATION: "ACCESS_COOKIE_EXPIRATION",
-            REFRESH_COOKIE_EXPIRATION: "REFRESH_COOKIE_EXPIRATION",
-
-            SUPABASE_URL: "SUPABASE_URL",
-            SUPABASE_KEY: "SUPABASE_KEY",
-            SUPABASE_JWT_SECRET: "SUPABASE_JWT_SECRET",
-
-            RMQ_URLS: "RMQ_URLS",
-
-            BOARDS_SERVICE_GRPC_URL: "BOARDS_SERVICE_GRPC_URL",
-            BOARDS_SERVICE_URL: "BOARDS_SERVICE_APP_URL",
-            BOARDS_SERVICE_RMQ_QUEUE: "BOARDS_SERVICE_RMQ_QUEUE",
-
-            FRONTEND_URL: "WEB_URL"
-        }
+const renamingMap: Record<string, Partial<Record<Apps, string>>> = {
+    // Gateway
+    GATEWAY_SERVICE_APP_URL: {
+        web: "VITE_API_URL"
     },
-    boards: {
-        title: "Boards Service",
-        destination: path.join(rootDir, "apps/boards/.env"),
-        variables: {
-            APP_PORT: "BOARDS_SERVICE_APP_PORT",
-            APP_URL: "BOARDS_SERVICE_APP_URL",
-
-            GRPC_URL: "BOARDS_SERVICE_GRPC_URL",
-
-            SQL_DB_NAME: "BOARDS_SERVICE_SQL_DB_NAME",
-            SQL_DB_HOST: "BOARDS_SERVICE_SQL_DB_HOST",
-            SQL_DB_PORT: "BOARDS_SERVICE_SQL_DB_PORT",
-            SQL_DB_USER: "BOARDS_SERVICE_SQL_DB_USER",
-            SQL_DB_PASS: "BOARDS_SERVICE_SQL_DB_PASS",
-
-            NOSQL_DB_NAME: "BOARDS_SERVICE_NOSQL_DB_NAME",
-            NOSQL_DB_HOST: "BOARDS_SERVICE_NOSQL_DB_HOST",
-            NOSQL_DB_PORT: "BOARDS_SERVICE_NOSQL_DB_PORT",
-
-            RMQ_URLS: "RMQ_URLS",
-            RMQ_QUEUE: "BOARDS_SERVICE_RMQ_QUEUE"
-        }
+    GATEWAY_SERVICE_APP_PORT: {
+        gateway: "APP_PORT"
     },
-    web: {
-        title: "Web",
-        destination: path.join(rootDir, "apps/web/.env"),
-        variables: {
-            VITE_APP_URL: "WEB_URL",
-            VITE_THEME_LS_KEY: "WEB_THEME_LS_KEY",
-            VITE_API_URL: "GATEWAY_SERVICE_APP_URL",
-            VITE_API_STALE_TIME: "WEB_API_STALE_TIME",
-            VITE_API_GC_TIME: "WEB_API_GC_TIME",
-            VITE_S3_REGION: "S3_REGION",
-            VITE_S3_ENDPOINT: "S3_ENDPOINT",
-            VITE_S3_BUCKET: "S3_BUCKET",
-            VITE_S3_ACCESS_KEY: "S3_ACCESS_KEY",
-            VITE_S3_SECRET_KEY: "S3_SECRET_KEY"
-        }
+
+    // // Cookie
+    COOKIE_HTTP_ONLY: {
+        gateway: "COOKIE_HTTP_ONLY"
+    },
+    COOKIE_SECURE: {
+        gateway: "COOKIE_SECURE"
+    },
+    ACCESS_COOKIE_EXPIRATION: {
+        gateway: "ACCESS_COOKIE_EXPIRATION"
+    },
+    REFRESH_COOKIE_EXPIRATION: {
+        gateway: "REFRESH_COOKIE_EXPIRATION"
+    },
+
+    // // Supabase
+    SUPABASE_URL: {
+        gateway: "SUPABASE_URL"
+    },
+    SUPABASE_KEY: {
+        gateway: "SUPABASE_KEY"
+    },
+    SUPABASE_DB_NAME: {
+        gateway: "SUPABASE_DB_NAME"
+    },
+    SUPABASE_DB_USER: {
+        gateway: "SUPABASE_DB_USER"
+    },
+    SUPABASE_DB_PASS: {
+        gateway: "SUPABASE_DB_PASS"
+    },
+    SUPABASE_JWT_SECRET: {
+        gateway: "SUPABASE_JWT_SECRET"
+    },
+    SUPABASE_JWT_EXPIRATION: {
+        gateway: "SUPABASE_JWT_EXPIRATION"
+    },
+    GOTRUE_PORT: {
+        gateway: "GOTRUE_PORT"
+    },
+
+    // Boards Service
+    BOARDS_SERVICE_APP_URL: {},
+    BOARDS_SERVICE_APP_PORT: {
+        boards: "APP_PORT"
+    },
+    BOARDS_SERVICE_GRPC_URL: {
+        gateway: "BOARDS_SERVICE_GRPC_URL",
+        boards: "GRPC_URL"
+    },
+
+    // // Postgres
+    BOARDS_SERVICE_SQL_DB_NAME: {
+        boards: "SQL_DB_NAME"
+    },
+    BOARDS_SERVICE_SQL_DB_HOST: {
+        boards: "SQL_DB_HOST"
+    },
+    BOARDS_SERVICE_SQL_DB_PORT: {
+        boards: "SQL_DB_PORT"
+    },
+    BOARDS_SERVICE_SQL_DB_USER: {
+        boards: "SQL_DB_USER"
+    },
+    BOARDS_SERVICE_SQL_DB_PASS: {
+        boards: "SQL_DB_PASS"
+    },
+
+    // // MongoDB
+    BOARDS_SERVICE_NOSQL_DB_NAME: {
+        boards: "NOSQL_DB_NAME"
+    },
+    BOARDS_SERVICE_NOSQL_DB_HOST: {
+        boards: "NOSQL_DB_HOST"
+    },
+    BOARDS_SERVICE_NOSQL_DB_PORT: {
+        boards: "NOSQL_DB_PORT"
+    },
+
+    // // RabbitMQ
+    BOARDS_SERVICE_RMQ_QUEUE: {
+        gateway: "BOARDS_SERVICE_RMQ_QUEUE",
+        boards: "RMQ_QUEUE"
+    },
+
+    // Media Service
+    MEDIA_SERVICE_APP_URL: {},
+    MEDIA_SERVICE_APP_PORT: {
+        media: "APP_PORT"
+    },
+    MEDIA_SERVICE_GRPC_URL: {
+        gateway: "MEDIA_SERVICE_GRPC_URL",
+        media: "GRPC_URL"
+    },
+
+    // // Postgres
+    MEDIA_SERVICE_SQL_DB_NAME: {
+        media: "SQL_DB_NAME"
+    },
+    MEDIA_SERVICE_SQL_DB_HOST: {
+        media: "SQL_DB_HOST"
+    },
+    MEDIA_SERVICE_SQL_DB_PORT: {
+        media: "SQL_DB_PORT"
+    },
+    MEDIA_SERVICE_SQL_DB_USER: {
+        media: "SQL_DB_USER"
+    },
+    MEDIA_SERVICE_SQL_DB_PASS: {
+        media: "SQL_DB_PASS"
+    },
+
+    // RabbitMQ
+    RMQ_URLS: {
+        gateway: "RMQ_URLS",
+        boards: "RMQ_URLS"
+    },
+    RMQ_PORT: {},
+    RMQ_MANAGEMENT_PORT: {},
+
+    // S3
+    S3_USER: {},
+    S3_PASS: {},
+    S3_REGION: {
+        media: "S3_REGION"
+    },
+    S3_ENDPOINT: {
+        media: "S3_ENDPOINT"
+    },
+    S3_BUCKET: {
+        media: "S3_BUCKET"
+    },
+    S3_PORT: {},
+    S3_UI_PORT: {},
+    S3_ACCESS_KEY: {
+        media: "S3_ACCESS_KEY"
+    },
+    S3_SECRET_KEY: {
+        media: "S3_SECRETE_KEY"
+    },
+    S3_URL_EXPIRATION: {
+        media: "S3_URL_EXPIRATION"
+    },
+
+    // Web
+    WEB_URL: {
+        gateway: "FRONTEND_URL",
+        web: "VITE_APP_URL"
+    },
+
+    // // Tanstack Query
+    WEB_API_STALE_TIME: {
+        web: "VITE_API_STALE_TIME"
+    },
+    WEB_API_GC_TIME: {
+        web: "VITE_S3_REGION"
+    },
+
+    // // LocalStorage
+    WEB_BOARD_LS_KEY: {},
+    WEB_THEME_LS_KEY: {
+        web: "VITE_THEME_LS_KEY"
     }
+};
+
+const contentsMap: Record<Apps, string> = {
+    gateway: "",
+    boards: "",
+    media: "",
+    web: ""
 };
 
 // Read .env file and create key/value record
 const envContent = fs.readFileSync(sourceEnvFile, "utf-8");
-const envRecord: Record<string, string> = {};
 
 for (const line of envContent.split("\n")) {
     if (line.includes("=")) {
@@ -88,25 +202,29 @@ for (const line of envContent.split("\n")) {
             throw new Error(`Invalid environment variable format: ${key}=${value}`);
         }
 
-        envRecord[key.trim()] = value.trim();
+        const renamingRecord = renamingMap[key];
+
+        if (!renamingRecord) {
+            throw new Error(`No renaming instructions for variable ${key}`);
+        }
+
+        for (const [app, newName] of Object.entries(renamingRecord)) {
+            contentsMap[app as Apps] += `${newName}=${value.trim()}\n`;
+        }
     }
 }
 
 // get corresponding variables and write them to corresponding files
-for (const { title, destination, variables } of Object.values(map)) {
-    let serviceEnvContent = "";
 
-    for (const [serviceKey, envKey] of Object.entries(variables)) {
-        const envValue = envRecord[envKey];
+const destinationsMap: Record<Apps, string> = {
+    gateway: path.join(rootDir, "apps/gateway/.env"),
+    boards: path.join(rootDir, "apps/boards/.env"),
+    media: path.join(rootDir, "apps/media/.env"),
+    web: path.join(rootDir, "apps/web/.env")
+};
 
-        if (!envValue) {
-            throw new Error(`Missing environment variable: ${envKey}`);
-        }
+for (const [app, destination] of Object.entries(destinationsMap)) {
+    fs.writeFileSync(destination, contentsMap[app as Apps]);
 
-        serviceEnvContent += `${serviceKey}=${envValue}\n`;
-    }
-
-    fs.writeFileSync(destination, serviceEnvContent);
-
-    console.info(`✅ Env file for '${title}' successfully loaded!`);
+    console.info(`✅ Env file for '${app}' successfully loaded!`);
 }
