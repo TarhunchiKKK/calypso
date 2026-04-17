12a552e3c2a0f5f28b16d3f2bd53b1ce643f10f2
TarhunchiKKK <kostabarilo12@gmail.com>
2026-04-18 01:58:45 +0300

    ci(apps/media): setup `Dockerfile`

--- apps/media/src/main.ts
+++ apps/media/src/main.ts
@@ -1,7 +1,7 @@
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import { type MicroserviceOptions, Transport } from "@nestjs/microservices";
-import { BOARDS_PACKAGE_NAME, GrpcLoaderOptions } from "@repo/api";
+import { GrpcLoaderOptions, MEDIA_PACKAGE_NAME } from "@repo/api";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
@@ -12,7 +12,7 @@ async function bootstrap() {
     app.connectMicroservice<MicroserviceOptions>({
         transport: Transport.GRPC,
         options: {
-            package: BOARDS_PACKAGE_NAME,
+            package: MEDIA_PACKAGE_NAME,
             protoPath: "node_modules/@repo/api/proto/media.proto",
             url: configService.getOrThrow<string>("GRPC_URL"),
             loader: GrpcLoaderOptions
