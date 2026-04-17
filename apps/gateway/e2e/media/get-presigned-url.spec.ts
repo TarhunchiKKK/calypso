7ca1e0176b5ee4b3e93f57fe02a3e03ff5e3a27e
TarhunchiKKK <kostabarilo12@gmail.com>
2026-04-17 17:48:37 +0300

    test(apps/gateway): tests for media microservice

---
+++ apps/gateway/e2e/media/get-presigned-url.spec.ts
@@ -0,0 +1,16 @@
+import { expect, test } from "@playwright/test";
+import type { GetPresignedUrlDto, MediaDomains } from "@repo/common";
+import { MediaApi } from "e2e/shared/api/media.api";
+
+test.describe("GET /media/presigned-url", () => {
+    test("success", async ({ request }) => {
+        const dto: GetPresignedUrlDto = {
+            fileName: "some-image.png",
+            contentType: "image"
+        };
+
+        const { response } = await MediaApi.getPresignedUrl(request, dto);
+
+        expect(response).toBeOK();
+    });
+});
