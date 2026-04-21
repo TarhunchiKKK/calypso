4f68226a44ae7a836a26164cadbb6a5dba315c01
TarhunchiKKK <kostabarilo12@gmail.com>
2026-04-21 11:21:05 +0300

    fix(apps/web): add 'username' field for `SignUpForm` component

--- apps/web/src/features/auth/ui/sign-up.form.tsx
+++ apps/web/src/features/auth/ui/sign-up.form.tsx
@@ -2,7 +2,14 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { type SignUpDto, SignUpDtoZodSchema } from "@repo/common";
 import { Controller, useForm } from "react-hook-form";
 import { toast } from "sonner";
-import { Button, Field, FieldError, FieldGroup, FieldLabel, Input } from "@/shared/ui/kit";
+import {
+    Button,
+    Field,
+    FieldError,
+    FieldGroup,
+    FieldLabel,
+    Input,
+} from "@/shared/ui/kit";
 import { AuthApi } from "../model/auth.api";
 
 type Props = {
@@ -12,15 +19,16 @@ type Props = {
 export function SignUpForm({ afterSubmit }: Props) {
     const form = useForm<SignUpDto>({
         defaultValues: {
+            username: "",
             email: "",
-            password: ""
+            password: "",
         },
-        resolver: zodResolver(SignUpDtoZodSchema)
+        resolver: zodResolver(SignUpDtoZodSchema),
     });
 
     const signUp = AuthApi.useSignUp();
 
-    const onSubmit = form.handleSubmit(async data => {
+    const onSubmit = form.handleSubmit(async (data) => {
         await signUp.mutateAsync(data);
 
         if (signUp.isError) {
@@ -35,15 +43,42 @@ export function SignUpForm({ afterSubmit }: Props) {
         <form onSubmit={onSubmit}>
             <FieldGroup>
                 <Controller
+                    name="username"
+                    control={form.control}
+                    render={({ field, fieldState }) => (
+                        <Field data-invalid={fieldState.invalid}>
+                            <FieldLabel>Username</FieldLabel>
+
+                            <Input
+                                {...field}
+                                aria-invalid={fieldState.invalid}
+                                placeholder="Nickname"
+                            />
+
+                            {fieldState.invalid && (
+                                <FieldError errors={[fieldState.error]} />
+                            )}
+                        </Field>
+                    )}
+                />
+
+                <Controller
                     name="email"
                     control={form.control}
                     render={({ field, fieldState }) => (
                         <Field data-invalid={fieldState.invalid}>
                             <FieldLabel>Email</FieldLabel>
 
-                            <Input {...field} aria-invalid={fieldState.invalid} placeholder="yourname@gmail.com" />
+                            <Input
+                                {...field}
+                                aria-invalid={fieldState.invalid}
+                                type="email"
+                                placeholder="yourname@gmail.com"
+                            />
 
-                            {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                            {fieldState.invalid && (
+                                <FieldError errors={[fieldState.error]} />
+                            )}
                         </Field>
                     )}
                 />
@@ -55,9 +90,16 @@ export function SignUpForm({ afterSubmit }: Props) {
                         <Field data-invalid={fieldState.invalid}>
                             <FieldLabel>Password</FieldLabel>
 
-                            <Input {...field} aria-invalid={fieldState.invalid} type="password" placeholder="********" />
+                            <Input
+                                {...field}
+                                aria-invalid={fieldState.invalid}
+                                type="password"
+                                placeholder="********"
+                            />
 
-                            {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                            {fieldState.invalid && (
+                                <FieldError errors={[fieldState.error]} />
+                            )}
                         </Field>
                     )}
                 />
