fc834d7438d96957666e3a05c75a3a584f1a3023
TarhunchiKKK <kostabarilo12@gmail.com>
2026-04-28 16:37:52 +0300

    refactor(apps/web): `ArrowsRelativePositionsMiddleware`

--- apps/web/src/board-editor/modules/arrows-resolution/arrows-relative-positions.middleware.ts
+++ apps/web/src/board-editor/modules/arrows-resolution/arrows-relative-positions.middleware.ts
@@ -2,62 +2,45 @@ import { NodesFactory } from "@/board-editor/nodes";
 import { NodeRectsFactory } from "@/board-editor/nodes/compose/factories/node-rects.factory";
 import type { NodesServiceMiddleware } from "@/entities/nodes";
 import { Geometry } from "@/shared/lib/geometry";
+import type { ArrowNode, NodeBase } from "@repo/boards-common";
 
 export const ARROW_RELATIVE_POSITIONS_MIDDLEWARE_KEY = Symbol();
 
-export const ArrowsRelativePositionsMiddleware: NodesServiceMiddleware = (nodes, payload) => {
+function getArrows(nodes: NodeBase[]): ArrowNode[] {
     const arrows = nodes.filter(node => node.type === "arrow");
 
     if (!NodesFactory.are(arrows, "arrow")) {
-        return nodes;
+        return [];
     }
 
+    return arrows;
+}
+
+export const ArrowsRelativePositionsMiddleware: NodesServiceMiddleware = (nodes, payload) => {
     switch (payload.operation) {
-        case "create": {
-            break;
-        }
-        case "update": {
-            for (const node of payload.nodes) {
-                const arrow = arrows.find(arrow => arrow.start.relativeTo === node.id || arrow.end.relativeTo === node.id);
-
-                if (!arrow) {
-                    continue;
-                }
-
-                const updatedNodeRect = NodeRectsFactory.rect(node);
-
-                for (const side of ["start", "end"] as const) {
-                    if (arrow[side].relativeTo === node.id) {
-                        const newPoint = Geometry.addPoints(arrow[side], updatedNodeRect);
-
-                        arrow[side] = {
-                            ...newPoint,
-                            relativeTo: node.id
-                        };
-                    }
-                }
-            }
-            break;
-        }
         case "remove": {
-            for (const removingNodeId of payload.nodes) {
-                const arrow = arrows.find(node => node.start.relativeTo === removingNodeId || node.end.relativeTo === removingNodeId);
+            const arrows = getArrows(nodes);
 
-                if (!arrow) {
-                    continue;
+            for (const nodeId of payload.nodes) {
+                const relatedArrows = arrows.filter(arrow => arrow.start.relativeTo === nodeId || arrow.end.relativeTo === nodeId);
+
+                if (relatedArrows.length === 0) {
+                    return nodes;
                 }
 
-                const removingNode = nodes.find(node => node.id === removingNodeId);
+                const removingNode = nodes.find(node => node.id === nodeId);
 
                 if (!removingNode) {
-                    throw Error(`Node with id ${removingNodeId} not found`);
+                    throw Error(`Node with id ${nodeId} not found`);
                 }
 
                 const removingNodeRect = NodeRectsFactory.rect(removingNode);
 
-                for (const side of ["start", "end"] as const) {
-                    if (arrow[side].relativeTo === removingNodeId) {
-                        arrow[side] = Geometry.addPoints(arrow[side], removingNodeRect);
+                for (const arrow of relatedArrows) {
+                    for (const side of ["start", "end"] as const) {
+                        if (arrow[side].relativeTo === nodeId) {
+                            arrow[side] = Geometry.addPoints(arrow[side], removingNodeRect);
+                        }
                     }
                 }
             }
