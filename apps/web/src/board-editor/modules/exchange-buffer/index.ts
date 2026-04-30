fffe322b1c1adb48073c1c6ddb18b2e3d1ffa15e
TarhunchiKKK <kostabarilo12@gmail.com>
2026-04-30 10:01:32 +0300

    fix(apps/web): `modules/exchange-buffer` structure

--- apps/web/src/board-editor/modules/exchange-buffer/index.ts
+++ apps/web/src/board-editor/modules/exchange-buffer/index.ts
@@ -1,1 +1,55 @@
-export { useExchangeBuffer } from "./hooks/use-exchange-buffer.hook";
+import type { NodeBase } from "@repo/boards-common";
+import type { Id, Point } from "@repo/common";
+import { useState } from "react";
+import { toast } from "sonner";
+import { NodeWrappersFactory } from "@/board-editor/nodes/compose/factories/node-wrappers.factory";
+import type { NodesService } from "@/entities/nodes";
+import { Geometry } from "@/shared/lib/geometry";
+import { calculateMiddlePoint } from "./geometry.lib";
+import { NodeClonesFactory } from "./node-clones.factory";
+
+export function useExchangeBuffer(nodes: NodeBase[], service: NodesService) {
+    const [selectedNodes, setSelectedNodes] = useState<NodeBase[]>();
+
+    const copy = (nodeIds: Set<Id>) => {
+        if (nodes.length === 0) {
+            toast.warning("No nodes selected");
+            return;
+        }
+
+        const nodesWithResolvedPositions = nodes
+            .filter(node => nodeIds.has(node.id))
+            .map(node => NodeWrappersFactory.wrap(nodes, node))
+            .map(wrapper => wrapper.data);
+
+        setSelectedNodes(nodesWithResolvedPositions);
+
+        toast.info("Nodes copied");
+    };
+
+    const paste = (pastePoint: Point) => {
+        if (!selectedNodes) {
+            toast.warning("No nodes selected");
+            return;
+        }
+
+        const middlePoint = calculateMiddlePoint(selectedNodes);
+        const offset = Geometry.calculateOffset(middlePoint, pastePoint);
+
+        const shiftedNodes = selectedNodes.map(node => NodeClonesFactory.clone(node, offset));
+
+        service.createMany(shiftedNodes);
+    };
+
+    const cut = (nodeIds: Set<Id>) => {
+        copy(nodeIds);
+
+        service.removeMany(new Set(nodes.map(node => node.id)));
+    };
+
+    return {
+        copy,
+        paste,
+        cut
+    };
+}
