a4051c649ce8f4be64e352ad5ccc7b62ab242fec
TarhunchiKKK <kostabarilo12@gmail.com>
2026-05-04 08:52:22 +0300

    fea(apps/web): add headers to `FormattableEditor`

--- apps/web/src/features/formatting/index.tsx
+++ apps/web/src/features/formatting/index.tsx
@@ -1,32 +1,51 @@
-import { BoldPlugin, ItalicPlugin, UnderlinePlugin } from "@platejs/basic-nodes/react";
+import { BlockquotePlugin, BoldPlugin, H1Plugin, H2Plugin, H3Plugin, ItalicPlugin, UnderlinePlugin } from "@platejs/basic-nodes/react";
 import type { Value } from "platejs";
 import { Plate, usePlateEditor } from "platejs/react";
-import { Editor, EditorContainer, FixedToolbar, MarkToolbarButton } from "@/features/formatting/ui";
+import { BlockquoteElement, Editor, EditorContainer, FixedToolbar, H1Element, H2Element, H3Element, MarkToolbarButton } from "@/features/formatting/ui";
+import { ToolbarButton } from "@/shared/ui/kit";
 
 const initialValue: Value = [
     {
-        type: "p",
+        children: [{ text: "Title" }],
+        type: "h3"
+    },
+    {
         children: [
-            { text: "Hello! Try out the " },
-            { text: "bold", bold: true },
-            { text: ", " },
-            { text: "italic", italic: true },
-            { text: ", and " },
-            { text: "underline", underline: true },
-            { text: " formatting." }
-        ]
+            {
+                children: [{ text: "This is a quote." }],
+                type: "p"
+            }
+        ],
+        type: "blockquote"
+    },
+    {
+        children: [{ text: "With some " }, { bold: true, text: "bold", underline: true }, { text: " text for emphasis!" }],
+        type: "p"
     }
 ];
 
-export function FormatableDocument() {
+export function FormattableDocument() {
     const editor = usePlateEditor({
-        plugins: [BoldPlugin, ItalicPlugin, UnderlinePlugin],
+        plugins: [
+            BoldPlugin,
+            ItalicPlugin,
+            UnderlinePlugin,
+            H1Plugin.withComponent(H1Element),
+            H2Plugin.withComponent(H2Element),
+            H3Plugin.withComponent(H3Element),
+            BlockquotePlugin.withComponent(BlockquoteElement)
+        ],
         value: initialValue
-    }); // Initializes the editor instance
+    });
 
     return (
-        <Plate editor={editor}>
+        <Plate editor={editor} onChange={console.log}>
             <FixedToolbar className="justify-start rounded-t-lg">
+                <ToolbarButton onClick={() => editor.tf.h1.toggle()}>H1</ToolbarButton>
+                <ToolbarButton onClick={() => editor.tf.h2.toggle()}>H2</ToolbarButton>
+                <ToolbarButton onClick={() => editor.tf.h3.toggle()}>H3</ToolbarButton>
+                <ToolbarButton onClick={() => editor.tf.blockquote.toggle()}>Quote</ToolbarButton>
+
                 <MarkToolbarButton nodeType="bold" tooltip="Bold (⌘+B)">
                     B
                 </MarkToolbarButton>
@@ -41,7 +60,7 @@ export function FormatableDocument() {
             {/* Provides editor context */}
             <EditorContainer>
                 {/* Styles the editor area */}
-                <Editor placeholder="Type your amazing content here..." />
+                <Editor placeholder="Type your amazing content here..."  />
             </EditorContainer>
         </Plate>
     );
