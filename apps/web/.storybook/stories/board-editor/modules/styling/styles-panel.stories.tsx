3f998ce6edcd8ff03ffa741b35eb37d9545e410d
TarhunchiKKK <kostabarilo12@gmail.com>
2026-05-06 15:45:39 +0300

    docs(app/web): story for `StylesPanel` component

---
+++ apps/web/.storybook/stories/board-editor/modules/styling/styles-panel.stories.tsx
@@ -0,0 +1,68 @@
+import type { Meta, StoryObj } from "@storybook/nextjs-vite";
+import { centered } from "#/lib/decorators";
+import { StylesPanel } from "@/board-editor/modules/styling";
+
+const meta = {
+    title: "Board Editor/Modules/Styling/Styles Panel",
+    component: StylesPanel
+} satisfies Meta<typeof StylesPanel>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Sticker: Story = {
+    decorators: centered,
+    args: {
+        type: "sticker",
+        update: () => {}
+    }
+};
+
+export const Arrow: Story = {
+    decorators: centered,
+    args: {
+        type: "arrow",
+        update: () => {}
+    }
+};
+
+export const Text: Story = {
+    decorators: centered,
+    args: {
+        type: "text",
+        update: () => {}
+    }
+};
+
+export const Shape: Story = {
+    decorators: centered,
+    args: {
+        type: "shape",
+        update: () => {}
+    }
+};
+
+export const Media: Story = {
+    decorators: centered,
+    args: {
+        type: "media",
+        update: () => {}
+    }
+};
+
+export const Note: Story = {
+    decorators: centered,
+    args: {
+        type: "note",
+        update: () => {}
+    }
+};
+
+export const NoNodeType: Story = {
+    decorators: centered,
+    args: {
+        type: null,
+        update: () => {}
+    }
+};
---
+++ apps/web/.storybook/stories/shared/ui/textarea-auto-font-size.component.story.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/nextjs-vite";
+import { centered } from "#/lib/decorators";
+import { TextareaAutoFontSize } from "@/shared/ui";
+
+const meta = {
+    title: "Shared/UI/Textarea Auto Font Size",
+    component: TextareaAutoFontSize
+} satisfies Meta<typeof TextareaAutoFontSize>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    decorators: centered,
+    args: {
+        value: "I am able t change font size automatically"
+    }
+};
