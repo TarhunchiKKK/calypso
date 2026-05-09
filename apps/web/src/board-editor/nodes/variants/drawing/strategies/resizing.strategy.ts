d9c617f06dfb271393cd91c0a9f6514aa9d55560
TarhunchiKKK <kostabarilo12@gmail.com>
2026-05-09 12:53:33 +0300

    feat(apps/web): `DrawingNodeResizingStrategy` class

---
+++ apps/web/src/board-editor/nodes/variants/drawing/strategies/resizing.strategy.ts
@@ -0,0 +1,26 @@
+import type { Decoratable } from "@/board-editor/core";
+import { NodeResizingStrategy } from "@/board-editor/modules/resizing";
+import type { DrawingNode } from "@repo/boards-common";
+import type { Point, Rect } from "@repo/common";
+
+export class DrawingNodeResizingStrategy extends NodeResizingStrategy {
+    public override updateNodeSizes(node: Decoratable<DrawingNode>, size: Rect) {
+        node.data = {
+            ...node.data,
+            rect: size,
+            points: this.scalePoints(node.data.points, node.data.rect, size)
+        };
+    }
+
+    private scalePoints(points: Point[], oldRect: Rect, newRect: Rect): Point[] {
+        const scale = {
+            x: oldRect.width === 0 ? newRect.width / oldRect.width : 1,
+            y: oldRect.height === 0 ? newRect.height / oldRect.height : 1
+        };
+
+        return points.map(point => ({
+            x: newRect.x + (point.x - oldRect.x) * scale.x,
+            y: newRect.y + (point.y - oldRect.y) * scale.y
+        }));
+    }
+}
