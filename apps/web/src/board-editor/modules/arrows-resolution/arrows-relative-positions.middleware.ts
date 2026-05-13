54f9d99d75e4834f8821b0010737f0283d50f6ad
Konstantin Barilo <kostabarilo12@gmail.com>
2026-05-13 10:50:51 +0000

    fix(apps/web): arrows unbinding

--- apps/web/src/board-editor/modules/arrows-resolution/arrows-relative-positions.middleware.ts
+++ apps/web/src/board-editor/modules/arrows-resolution/arrows-relative-positions.middleware.ts
@@ -1,6 +1,5 @@
 import type { ArrowNode, NodeBase } from "@repo/boards-common";
 import { NodeRectsFactory, NodesFactory, type NodesServiceMiddleware } from "@/entities/nodes";
-import { Geometry } from "@/shared/lib/geometry";
 
 export const ARROW_RELATIVE_POSITIONS_MIDDLEWARE_KEY = Symbol();
 
@@ -44,7 +43,10 @@ export const ArrowsRelativePositionsMiddleware: NodesServiceMiddleware = (nodes,
                 for (const arrow of relatedArrows) {
                     for (const side of ["start", "end"] as const) {
                         if (arrow[side].relativeTo === nodeId) {
-                            arrow[side] = Geometry.addPoints(arrow[side], removingNodeRect);
+                            arrow[side] = {
+                                x: removingNodeRect.x + removingNodeRect.width * arrow[side].x,
+                                y: removingNodeRect.y + removingNodeRect.height * arrow[side].y
+                            };
                         }
                     }
                 }
