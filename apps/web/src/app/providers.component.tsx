2c02df93e23b5eeebe37efa93bb50f97737a7a05
TarhunchiKKK <kostabarilo12@gmail.com>
2026-05-22 00:30:30 +0300

    ui(apps/web): `ErrorBoundaryFallback`

--- apps/web/src/app/providers.component.tsx
+++ apps/web/src/app/providers.component.tsx
@@ -1,17 +1,25 @@
-import { QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider, QueryErrorResetBoundary } from "@tanstack/react-query";
 import type { PropsWithChildren } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { ThemeProvider } from "@/features/dark-mode";
 import { QueryClientInstance } from "@/shared/config";
+import { ErrorBoundaryFallback } from "@/shared/ui";
 import { Toaster } from "@/shared/ui/kit";
 
 export function Providers({ children }: PropsWithChildren) {
     return (
         <QueryClientProvider client={QueryClientInstance}>
-            <ThemeProvider>
-                {children}
+            <QueryErrorResetBoundary>
+                {({ reset }) => (
+                    <ErrorBoundary onReset={reset} fallbackRender={({ resetErrorBoundary }) => <ErrorBoundaryFallback onClick={resetErrorBoundary} />}>
+                        <ThemeProvider>
+                            {children}
 
-                <Toaster />
-            </ThemeProvider>
+                            <Toaster />
+                        </ThemeProvider>
+                    </ErrorBoundary>
+                )}
+            </QueryErrorResetBoundary>
         </QueryClientProvider>
     );
 }
--- apps/web/src/shared/ui/index.ts
+++ apps/web/src/shared/ui/index.ts
@@ -1,4 +1,5 @@
 export * from "./dropdown.component";
+export * from "./error-boundary.fallback";
 export * from "./logo.component";
 export * from "./textarea-auto-font-size.component";
 export * from "./wrapper.component";
