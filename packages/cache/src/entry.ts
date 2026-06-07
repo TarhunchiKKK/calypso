9188185a85af977a78ae3915ceea5534c2e313ca
TarhunchiKKK <kostabarilo12@gmail.com>
2026-06-07 19:34:11 +0300

    feat(packages/cache): create `ManualCaching` decorator

---
+++ packages/cache/src/entry.ts
@@ -0,0 +1,5 @@
+export { CacheModule } from "./cache.module";
+export { CacheService } from "./cache.service";
+export { Cache } from "./decorators/cache.decorator";
+export { InvalidateCache } from "./decorators/invalidate-cache.decorator";
+export { ManualCaching } from "./metadata/manual-caching.decorator";
