1cc241ee0b92553cdbbf1aded71803ea012e0c4c
Konstantin Barilo <kostabarilo12@gmail.com>
2026-06-08 12:20:37 +0000

    chore: `WebKeys` constant

---
+++ scripts/load-env/web.constants.ts
@@ -0,0 +1,15 @@
+export const WebKeys = {
+    WEB_URL: "VITE_APP_URL",
+
+    // S3
+    S3_ENDPOINT: "VITE_S3_ENDPOINT",
+    S3_BUCKET: "VITE_S3_BUCKET",
+    S3_REGION: "VITE_S#_REGION",
+
+    // Queries
+    WEB_API_STALE_TIME: "VITE_API_STALE_TIME",
+    WEB_API_GC_TIME: "VITE_API_GC_TIME",
+
+    // LocalStorage
+    WEB_THEME_LS_KEY: "VITE_THEME_LS_KEY"
+};
