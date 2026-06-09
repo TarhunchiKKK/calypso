50ec3b8239c6ca7a11c18b567236efae88e2fb9f
Konstantin Barilo <kostabarilo12@gmail.com>
2026-06-09 06:01:48 +0000

    refactor(packages/api): change project structure

--- packages/api/src/entry.ts
+++ packages/api/src/entry.ts
@@ -2,5 +2,5 @@ export * from "./broker";
 export * from "./config";
 export * from "./core";
 export * from "./modules/access-rights";
-export * from "./shared/swagger";
-export * from "./shared/validation";
+export * from "./swagger";
+export * from "./validation";
