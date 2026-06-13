3eb3cea81ee3f1121a9f4019678ea137034cf9b0
TarhunchiKKK <kostabarilo12@gmail.com>
2026-06-13 22:28:54 +0300

    test(boards): `FindAllBoardsQueryHandler` class

---
+++ apps/boards/test/unit/boards/find-all-boards.handler.spec.ts
@@ -0,0 +1,65 @@
+import { afterEach, beforeEach, describe, expect, it } from "bun:test";
+import { clearMock, createRepositoryMock } from "@api/common";
+import type { PaginationOptions } from "@lib/common";
+import type { ProjectFilters } from "@lib/projects";
+import { Test, type TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { Board } from "src/boards/entities/board.entity";
+import { FindAllBoardsQuery, FindAllBoardsQueryHandler } from "src/boards/handlers/find-all-boards.handler";
+
+describe("FindAllBoardsQueryHandler", () => {
+    let handler: FindAllBoardsQueryHandler;
+
+    const boardsRepositoryMock = createRepositoryMock();
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                FindAllBoardsQueryHandler,
+                {
+                    provide: getRepositoryToken(Board),
+                    useValue: boardsRepositoryMock
+                }
+            ]
+        }).compile();
+
+        handler = module.get(FindAllBoardsQueryHandler);
+    });
+
+    afterEach(() => {
+        clearMock(boardsRepositoryMock);
+    });
+
+    it("should find boards", async () => {
+        const userId = crypto.randomUUID();
+
+        const filters: ProjectFilters = {
+            type: "board",
+            sortOrder: "alphabetic",
+            own: true
+        };
+
+        const pagination: PaginationOptions = {
+            page: 0,
+            count: 100
+        };
+
+        const boards: Board[] = [
+            {
+                id: crypto.randomUUID(),
+                title: "Board",
+                icon: "",
+                creatorId: userId,
+                createdAt: new Date()
+            }
+        ];
+
+        boardsRepositoryMock.find.mockResolvedValue(boards);
+
+        const query = new FindAllBoardsQuery(userId, filters, pagination);
+
+        const result = await handler.execute(query);
+
+        expect(result).toEqual(boards);
+    });
+});
