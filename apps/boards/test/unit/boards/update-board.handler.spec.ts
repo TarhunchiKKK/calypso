cac76d086f5784ef9ec3cc0503a7807cdb0a2187
TarhunchiKKK <kostabarilo12@gmail.com>
2026-06-13 22:50:52 +0300

    test(boards): `UpdateBoardCommandHandler` class

---
+++ apps/boards/test/unit/boards/update-board.handler.spec.ts
@@ -0,0 +1,58 @@
+import { afterEach, beforeEach, describe, expect, it } from "bun:test";
+import { clearMock, createRepositoryMock } from "@api/common";
+import type { UpdateBoardDto } from "@lib/boards";
+import { Test, type TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { Board } from "src/boards/entities/board.entity";
+import { UpdateBoardCommand, UpdateBoardCommandHandler } from "src/boards/handlers/update-board.handler";
+import { BoardsHelper } from "src/boards/lib/boards.helper";
+import { createBoardsHelperMock } from "./mocks";
+import { MockBoard } from "./mocks/board.mocks";
+
+describe("UpdateBoardCommandHandler", () => {
+    let handler: UpdateBoardCommandHandler;
+
+    const boardsRepositoryMock = createRepositoryMock();
+
+    const boardsHelperMock = createBoardsHelperMock();
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                UpdateBoardCommandHandler,
+                {
+                    provide: BoardsHelper,
+                    useValue: boardsHelperMock
+                },
+                {
+                    provide: getRepositoryToken(Board),
+                    useValue: boardsRepositoryMock
+                }
+            ]
+        }).compile();
+
+        handler = module.get(UpdateBoardCommandHandler);
+    });
+
+    afterEach(() => {
+        clearMock(boardsHelperMock);
+        clearMock(boardsRepositoryMock);
+    });
+
+    it("should update board", async () => {
+        const dto: UpdateBoardDto = {
+            title: "New title",
+            description: "New description",
+            icon: "new-icon-path.png"
+        };
+
+        boardsHelperMock.findOneById.mockResolvedValue(MockBoard);
+
+        const command = new UpdateBoardCommand(MockBoard.id, dto);
+
+        const result = await handler.execute(command);
+
+        expect(result).toEqual(MockBoard.id);
+        expect(boardsHelperMock.findOneById).toHaveBeenCalledWith(MockBoard.id);
+    });
+});
