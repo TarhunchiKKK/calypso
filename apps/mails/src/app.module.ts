50fc2304f6b46b98d38c156af5cd9e1ad3b365ac
TarhunchiKKK <kostabarilo12@gmail.com>
2026-06-13 11:52:40 +0300

    fix(mails): add `CacheModule` into root `AppModule`

--- apps/mails/src/app.module.ts
+++ apps/mails/src/app.module.ts
@@ -1,10 +1,21 @@
+import { CacheModule, cacheConfigFactory } from "@api/cache";
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { CqrsModule } from "@nestjs/cqrs";
 import { AuthModule } from "./modules/auth/auth.module";
 import { MailsModule } from "./shared/mails/mails.module";
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true }), CqrsModule.forRoot(), MailsModule, AuthModule]
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        CqrsModule.forRoot(),
+        CacheModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: cacheConfigFactory
+        }),
+        MailsModule,
+        AuthModule
+    ]
 })
 export class AppModule {}
