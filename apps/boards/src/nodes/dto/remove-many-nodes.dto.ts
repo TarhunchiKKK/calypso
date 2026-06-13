47a6575e6c283400d9d29107bcf6e6fda36ef352
TarhunchiKKK <kostabarilo12@gmail.com>
2026-06-13 23:14:48 +0300

    test(boards): `CreateManyNodesCommandHandler` class

--- apps/boards/src/nodes/dto/remove-many-nodes.dto.ts
+++ apps/boards/src/nodes/dto/remove-many-nodes.dto.ts
@@ -1,5 +1,6 @@
 import type { Id } from "@lib/common";
 
+// DELETE: corresponding type exists in `@lib/boards`
 export type RemoveManyNodesDto = {
     ids: Id[];
 
--- apps/boards/test/unit/nodes/create-many-nodes.handler.spec.ts
+++ apps/boards/test/unit/nodes/create-many-nodes.handler.spec.ts
@@ -1,21 +1,46 @@
-import { afterEach, beforeEach, describe, it } from "bun:test";
+import { afterEach, beforeEach, describe, expect, it } from "bun:test";
+import { clearMock, createMongooseModelMock } from "@api/common";
+import type { CreateManyNodesDto } from "@lib/boards";
+import { getModelToken } from "@nestjs/mongoose";
 import { Test, type TestingModule } from "@nestjs/testing";
-import { CreateManyNodesCommandHandler } from "src/nodes/handlers/create-many-nodes.handler";
+import { CreateManyNodesCommand, CreateManyNodesCommandHandler } from "src/nodes/handlers/create-many-nodes.handler";
+import { NodeBase } from "src/nodes/schemas/node-base.schema";
+import { MockBoard } from "../boards/mocks/board.mocks";
+import { MockNodes } from "./mocks";
 
 describe("CreateManyNodesCommandHandler", () => {
     let handler: CreateManyNodesCommandHandler;
 
+    const nodesModelMock = createMongooseModelMock();
+
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [CreateManyNodesCommandHandler]
+            providers: [
+                CreateManyNodesCommandHandler,
+                {
+                    provide: getModelToken(NodeBase.name),
+                    useValue: nodesModelMock
+                }
+            ]
         }).compile();
 
         handler = module.get(CreateManyNodesCommandHandler);
     });
 
     afterEach(() => {
-        // clearMock();
+        clearMock(nodesModelMock);
     });
 
-    it("", async () => {});
+    it("should create nodes", async () => {
+        const dto: CreateManyNodesDto = {
+            boardId: MockBoard.id,
+            nodes: [MockNodes.sticker, MockNodes.arrow]
+        };
+
+        const command = new CreateManyNodesCommand(dto);
+
+        await handler.execute(command);
+
+        expect(nodesModelMock.insertMany).toHaveBeenCalled();
+    });
 });
