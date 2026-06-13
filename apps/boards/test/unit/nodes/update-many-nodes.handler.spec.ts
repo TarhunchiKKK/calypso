75e9e8bc2b398aad0347d4abd35b53133b7afa6d
TarhunchiKKK <kostabarilo12@gmail.com>
2026-06-13 23:18:51 +0300

    test(boards): `UpdateManyNodesCommandHandler` class

---
+++ apps/boards/test/unit/nodes/update-many-nodes.handler.spec.ts
@@ -0,0 +1,46 @@
+import { afterEach, beforeEach, describe, expect, it } from "bun:test";
+import { clearMock, createMongooseModelMock } from "@api/common";
+import type { UpdateManyNodesDto } from "@lib/boards";
+import { getModelToken } from "@nestjs/mongoose";
+import { Test, type TestingModule } from "@nestjs/testing";
+import { UpdateManyNodesCommand, UpdateManyNodesCommandHandler } from "src/nodes/handlers/update-many-nodes.handler";
+import { NodeBase } from "src/nodes/schemas/node-base.schema";
+import { MockBoard } from "../boards/mocks/board.mocks";
+import { MockNodes } from "./mocks";
+
+describe("UpdateManyNodesCommandHandler", () => {
+    let handler: UpdateManyNodesCommandHandler;
+
+    const nodesModelMock = createMongooseModelMock();
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                UpdateManyNodesCommandHandler,
+                {
+                    provide: getModelToken(NodeBase.name),
+                    useValue: nodesModelMock
+                }
+            ]
+        }).compile();
+
+        handler = module.get(UpdateManyNodesCommandHandler);
+    });
+
+    afterEach(() => {
+        clearMock(nodesModelMock);
+    });
+
+    it("should replace nodes", async () => {
+        const dto: UpdateManyNodesDto = {
+            boardId: MockBoard.id,
+            nodes: [MockNodes.sticker, MockNodes.arrow]
+        };
+
+        const command = new UpdateManyNodesCommand(dto);
+
+        await handler.execute(command);
+
+        expect(nodesModelMock.bulkWrite).toHaveBeenCalled();
+    });
+});
--- packages/api/common/src/mocks/mongoose.mocks.ts
+++ packages/api/common/src/mocks/mongoose.mocks.ts
@@ -2,11 +2,12 @@ import { mock } from "bun:test";
 import type { WithMockedMethods } from "entry";
 import type { Model } from "mongoose";
 
-type ModelFields = "find" | "insertMany";
+type ModelFields = "find" | "insertMany" | "bulkWrite";
 
 export function createMongooseModelMock() {
     return {
         find: mock(() => Promise.resolve({})),
-        insertMany: mock(() => Promise.resolve({}))
+        insertMany: mock(() => Promise.resolve({})),
+        bulkWrite: mock(() => Promise.resolve({}))
     } satisfies WithMockedMethods<Pick<Model<any>, ModelFields>>;
 }
