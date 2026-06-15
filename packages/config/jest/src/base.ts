b1db210fdaa3b21366196da389a534004d6d1f81
TarhunchiKKK <kostabarilo12@gmail.com>
2026-06-15 14:42:08 +0300

    fix: remove `@config/jest` package

--- packages/config/jest/src/base.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "jest";
-
-export const baseConfig = {
-    collectCoverage: true,
-    coverageDirectory: "coverage",
-    coverageProvider: "v8",
-    moduleFileExtensions: ["js", "ts", "json", "tsx"],
-    testEnvironment: "jsdom"
-} as const satisfies Config;
