aa9c700f137856557407335a4f60c322ebf23b28
TarhunchiKKK <kostabarilo12@gmail.com>
2026-06-18 12:39:51 +0300

    chore: update `env:refresh-example` script

--- scripts/refresh-env-example.script.ts
+++ scripts/refresh-env-example.script.ts
@@ -1,34 +1,61 @@
-import fs from "node:fs";
-import path from "node:path";
+import * as fs from "node:fs";
+import * as path from "node:path";
 
 const rootDir = process.cwd();
-const source = ".env";
-const destination = ".env.example";
 
-const exclude: string[] = ["MAIL_HOST", "MAIL_PORT", "MAIL_LOGIN", "MAIL_PASSWORD", "MAIL_KEY"];
+const apps = [
+    {
+        folder: rootDir,
+        exclude: []
+    },
 
-const content = fs.readFileSync(path.join(rootDir, source), "utf-8");
-let result = "";
+    {
+        folder: path.join(rootDir, "apps/gateway"),
+        exclude: []
+    },
+    {
+        folder: path.join(rootDir, "apps/boards"),
+        exclude: []
+    },
+    {
+        folder: path.join(rootDir, "apps/media"),
+        exclude: []
+    },
+    {
+        folder: path.join(rootDir, "apps/mails"),
+        exclude: ["MAIL_HOST", "MAIL_PORT", "MAIL_LOGIN", "MAIL_PASSWORD", "MAIL_KEY"]
+    },
+    {
+        folder: path.join(rootDir, "apps/web"),
+        exclude: []
+    }
+];
 
-for (const line of content.split("\n")) {
-    if (!line.includes("=")) {
-        result += `${line}\n`;
-        continue;
+for (const { folder, exclude } of apps) {
+    const content = fs.readFileSync(path.join(folder, ".env"), "utf-8");
+
+    let result = "";
+
+    for (const line of content.split("\n")) {
+        if (!line.includes("=")) {
+            result += `${line}\n`;
+            continue;
+        }
+
+        const [key, value] = line.split("=");
+
+        if (!key || !value) {
+            throw new Error(`Invalid environment variable format: ${key}=${value}`);
+        }
+
+        if (exclude.includes(key)) {
+            result += `${key}=\n`;
+        } else {
+            result += `${key}=${value}\n`;
+        }
     }
 
-    const [key, value] = line.split("=");
+    fs.writeFileSync(path.join(folder, ".env.example"), result);
 
-    if (!key || !value) {
-        throw new Error(`Invalid environment variable format: ${key}=${value}`);
-    }
-
-    if (exclude.includes(key)) {
-        result += `${key}=\n`;
-    } else {
-        result += `${key}=${value}\n`;
-    }
+    console.info(`✅ '${path.join(folder, ".env.example")}' successfully loaded!`);
 }
-
-fs.writeFileSync(path.join(rootDir, destination), result);
-
-console.info(`✅ '${destination}' successfully loaded!`);
