26c07a4276a47ed390f8c10dcb13406137963291
TarhunchiKKK <kostabarilo12@gmail.com>
2026-06-19 10:51:40 +0300

    impl(contracts/grpc): implement logging in  `GrpcExceptionFilter`

--- packages/contracts/grpc/src/middleware/compose.decorator.ts
+++ packages/contracts/grpc/src/middleware/compose.decorator.ts
@@ -1,5 +1,5 @@
 import { applyDecorators, Controller, UseFilters, UseInterceptors } from "@nestjs/common";
-import { ExtractGrpcInterceptor } from "./await-grpc.interceptor";
+import { ExtractGrpcInterceptor } from "./extract-grpc.interceptor";
 import { GrpcExceptionFilter } from "./grpc-exception.filter";
 import { WrapGrpcResponseInterceptor } from "./wrap-grpc-response.interceptor";
 
--- packages/contracts/grpc/src/middleware/grpc-exception.filter.ts
+++ packages/contracts/grpc/src/middleware/grpc-exception.filter.ts
@@ -1,4 +1,5 @@
-import { Catch, type HttpException, HttpStatus, Logger } from "@nestjs/common";
+import { AppLogger } from "@api/logs";
+import { type ArgumentsHost, Catch, type HttpException, HttpStatus, Inject } from "@nestjs/common";
 import { BaseRpcExceptionFilter } from "@nestjs/microservices";
 import { from, type Observable } from "rxjs";
 import type { GrpcError } from "../generated";
@@ -6,31 +7,45 @@ import type { GrpcResponse } from "../types";
 
 @Catch()
 export class GrpcExceptionFilter extends BaseRpcExceptionFilter {
-    public catch(exception: HttpException): Observable<GrpcResponse> {
-        Logger.error(exception);
+    protected readonly contextName = GrpcExceptionFilter.name;
 
+    public constructor(@Inject(AppLogger) private readonly logger: AppLogger) {
+        super();
+    }
+
+    public catch(exception: HttpException, host: ArgumentsHost): Observable<GrpcResponse> {
+        const error = this.extractError(exception);
+
+        this.handleError(error, host);
+
+        return from([{ error }]);
+    }
+
+    private extractError(exception: HttpException): GrpcError {
         if ("getResponse" in exception) {
             const { message, error, statusCode } = exception.getResponse() as GrpcError;
 
-            return from([
-                {
-                    error: {
-                        message: Array.isArray(message) ? message : [message],
-                        error,
-                        statusCode
-                    }
-                }
-            ]);
+            return {
+                message: Array.isArray(message) ? message : [message],
+                error,
+                statusCode
+            };
         }
 
-        return from([
-            {
-                error: {
-                    message: ["Unknown error"],
-                    error: "Unknown error",
-                    statusCode: HttpStatus.BAD_REQUEST
-                }
-            }
-        ]);
+        return {
+            message: ["Unknown error"],
+            error: "Unknown error",
+            statusCode: HttpStatus.INTERNAL_SERVER_ERROR
+        };
+    }
+
+    private handleError(error: GrpcError, host: ArgumentsHost) {
+        if (error.statusCode >= HttpStatus.INTERNAL_SERVER_ERROR) {
+            const rpc = host.switchToRpc();
+            const data = rpc.getData;
+            const context = rpc.getContext();
+
+            this.logger.error(`Error in gRPC-handler: ${error}`, { context: this.contextName, data, grpcContext: context });
+        }
     }
 }
