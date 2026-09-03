5fd08c10bce68f4a424d3e4dafe401e793da106f
TarhunchiKKK <kostabarilo12@gmail.com>
2026-09-03 15:23:07 +0300

    feat(api): add modules configuration for `AppModule` class

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { CqrsModule } from "@nestjs/cqrs";
+import { MongooseModule } from "@nestjs/mongoose";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { CacheConfigService, MongooseConfigService, TypeOrmConfigService } from "./config";
 import { CacheModule } from "./infra/cache/cache.module";
+import { BasicAuthModule } from "./modules/auth/basic/basic-auth.module";
+import { EmailVerificationModule } from "./modules/auth/email-verification/email-verification.module";
+import { PasswordRecoveryModule } from "./modules/auth/password-recovery/password-recovery.module";
+import { UsersModule } from "./modules/auth/users/users.module";
+import { BoardsModule } from "./modules/boards/boards.module";
 import { MediaModule } from "./modules/media/media.module";
+import { NodesModule } from "./modules/nodes/nodes.module";
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true }), CqrsModule.forRoot(), MediaModule, CacheModule]
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        CqrsModule.forRoot(),
+        TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+        MongooseModule.forRootAsync({ useClass: MongooseConfigService }),
+        CacheModule.forRootAsync({ useClass: CacheConfigService }),
+        UsersModule,
+        BasicAuthModule,
+        EmailVerificationModule,
+        PasswordRecoveryModule,
+        BoardsModule,
+        NodesModule,
+        MediaModule
+    ]
 })
 export class AppModule {}
