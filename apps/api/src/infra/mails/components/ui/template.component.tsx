f8c163a7c0402e41f12a68496cb7a5848894f5d1
Konstantin Barilo <kostabarilo12@gmail.com>
2026-09-04 09:04:20 +0000

    fix(api): fix `@react-email` jsx parsing

--- apps/api/src/infra/mails/components/ui/template.component.tsx
+++ apps/api/src/infra/mails/components/ui/template.component.tsx
@@ -1,5 +1,5 @@
 import { Body, Head, Html, Preview, Tailwind } from "@react-email/components";
-// biome-ignore lint/style/useImportType: This is necessary for jsx parsing
+// biome-ignore lint/style/useImportType: This is necessary for valid jsx parsing
 import * as React from "react";
 import { SupportSection } from "./support-section.component";
 
